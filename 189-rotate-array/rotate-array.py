366c41f1532873f5434a6d84ecce71ee2991c7f3
GautamShah140404 <gautamshah0104@gmail.com>
2025-05-12 10:20:03 +0530

    Time: 1 ms (75.75%) | Memory: 22.8 MB (28.04%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -6,13 +6,9 @@ class Solution(object):
         :rtype: None (modifies nums in-place)
         """
         n = len(nums)
-        k = k % n  # Handle cases where k > n
-        
-        # Step 1: Reverse the entire array
+        k = k%n
         nums.reverse()
         
-        # Step 2: Reverse first k elements
         nums[:k] = reversed(nums[:k])
         
-        # Step 3: Reverse the remaining elements
         nums[k:] = reversed(nums[k:])
